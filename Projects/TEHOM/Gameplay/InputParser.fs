52c295bb51596612918464600a6f925680d7589d
yvileapsis <yvileapsis@gmail.com>
2024-01-29 23:48:23 +0300

    wip

--- Projects/TEHOM/Gameplay/InputParser.fs
+++ Projects/TEHOM/Gameplay/InputParser.fs
@@ -1,23 +1,335 @@
 namespace Tehom
 
-open Nu
-open FParsec
+module NuMark =
 
-open FSharp.Formatting.Markdown
-open FSharp.Formatting.Common
+    #r "nuget: FParsec"
+    open FParsec
+    (*
 
-module Testing =
-    let document =
-        """
-# F# Hello world
-Hello world in [F#](http://fsharp.net) looks like this:
+    Regular Markdown has very limited styling capabilities and is often paired with bbcode/html/ansi code to fix it.
+    The result is not very cohesive and quite annoying to write.
 
-    printfn "Hello world!"
+    What we're going to leverage for this markup language is that majority of formating is done on per-line basis.
+    *)
 
-For more see [fsharp.org][fsorg].
+    type Color = Color
 
-  [fsorg]: http://fsharp.org "The F# organization." """
+    type Style = {
+        Size: uint
+        Style: uint
+        Color: Color
+    }
 
-    let parsed = Markdown.Parse(document)
+    type Justification = Justification
 
-    parsed.Paragraphs.Head
+    type Text =
+        | Normal of Text list
+        | Bold of Text list
+        | Italics of Text list
+        | Strikethrough of Text list
+        | Underlined of Text list
+        | Subscript of Text list
+        | Superscript of Text list
+        | Color of string * Text
+        | String of string
+
+    let lineParser : Parser<Text list, unit> =
+        let lineElements, lineElementsR = createParserForwardedToRef()
+
+        let specialChars = "~^*_"
+
+        let text = many1Satisfy (isNoneOf specialChars) |>> Text.String
+        let trailing = many1Satisfy (isAnyOf specialChars) |>> Text.String
+
+        let enclosed left right =
+            pstring left >>.
+            many1Till lineElements (followedByString right)
+            .>> pstring right
+
+        let style style syntax =
+            syntax
+            |> Seq.map (fun s -> enclosed s s |>> style)
+            |> choice
+            |> attempt
+            <?> nameof style
+
+
+        let lineElement : Parser<Text, unit> = choice [
+            text
+            style Bold ["**"]
+            style Italics ["*"]
+            style Strikethrough ["~~"]
+            style Normal ["~"]
+            style Underlined ["__"]
+            style Subscript ["_"]
+            style Superscript ["^"]
+            trailing
+        ]
+
+        lineElementsR.Value <- lineElement
+
+        many lineElements
+
+
+    run lineParser """~~**This is bold text** __This is underlined text__~~ *This is italic text* _This is subscript text_ ~~Strikethrough~~ ~This is Normal Text~"""
+
+
+    type Node =
+        | Header of Node
+        | Quote of Node
+        | Indent of Node
+        | List of Node
+        | Code
+        | Codeblock
+        | Table
+        | Image
+        | HorizontalLine
+        | Line of Justification * Text
+
+
+    let textTest  =
+        """---
+__Advertisement :)__
+
+- __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image
+  resize in browser.
+- __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly
+  i18n with plurals support and easy syntax.
+
+You will like those projects!
+
+---
+
+# h1 Heading 8-)
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+
+## Horizontal Rules
+
+___
+
+---
+
+***
+
+
+## Typographic replacements
+
+Enable typographer option to see result.
+
+(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+
+test.. test... test..... test?..... test!....
+
+!!!!!! ???? ,,  -- ---
+
+"Smartypants, double quotes" and 'single quotes'
+
+
+## Emphasis
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+~~Strikethrough~~
+
+
+## Blockquotes
+
+
+> Blockquotes can also be nested...
+>> ...by using additional greater-than signs right next to each other...
+> > > ...or with spaces between arrows.
+
+
+## Lists
+
+Unordered
+
++ Create a list by starting a line with `+`, `-`, or `*`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as `1.`
+
+Start numbering with offset:
+
+57. foo
+1. bar
+
+
+## Code
+
+Inline `code`
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
+
+```
+Sample text here...
+```
+
+Syntax highlighting
+
+``` js
+var foo = function (bar) {
+  return bar++;
+};
+
+console.log(foo(5));
+```
+
+## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+Right aligned columns
+
+| Option | Description |
+| ------:| -----------:|
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+
+## Links
+
+[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
+
+
+## Images
+
+![Minion](https://octodex.github.com/images/minion.png)
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
+
+Like links, Images also have a footnote style syntax
+
+![Alt text][id]
+
+With a reference later in the document defining the URL location:
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+
+
+## Plugins
+
+The killer feature of `markdown-it` is very effective support of
+[syntax plugins](https://www.npmjs.org/browse/keyword/markdown-it-plugin).
+
+
+### [Emojies](https://github.com/markdown-it/markdown-it-emoji)
+
+> Classic markup: :wink: :cry: :laughing: :yum:
+>
+> Shortcuts (emoticons): :-) :-( 8-) ;)
+
+see [how to change output](https://github.com/markdown-it/markdown-it-emoji#change-output) with twemoji.
+
+
+### [Subscript](https://github.com/markdown-it/markdown-it-sub) / [Superscript](https://github.com/markdown-it/markdown-it-sup)
+
+- 19^th^
+- H~2~O
+
+
+### [\<ins>](https://github.com/markdown-it/markdown-it-ins)
+
+++Inserted text++
+
+
+### [\<mark>](https://github.com/markdown-it/markdown-it-mark)
+
+==Marked text==
+
+
+### [Footnotes](https://github.com/markdown-it/markdown-it-footnote)
+
+Footnote 1 link[^first].
+
+Footnote 2 link[^second].
+
+Inline footnote^[Text of inline footnote] definition.
+
+Duplicated footnote reference[^second].
+
+[^first]: Footnote **can have markup**
+
+    and multiple paragraphs.
+
+[^second]: Footnote text.
+
+
+### [Definition lists](https://github.com/markdown-it/markdown-it-deflist)
+
+Term 1
+
+:   Definition 1
+with lazy continuation.
+
+Term 2 with *inline markup*
+
+:   Definition 2
+
+        { some code, part of Definition 2 }
+
+    Third paragraph of definition 2.
+
+_Compact style:_
+
+Term 1
+  ~ Definition 1
+
+Term 2
+  ~ Definition 2a
+  ~ Definition 2b
+
+
+### [Abbreviations](https://github.com/markdown-it/markdown-it-abbr)
+
+This is HTML abbreviation example.
+
+It converts "HTML", but keep intact partial entries like "xxxHTMLyyy" and so on.
+
+*[HTML]: Hyper Text Markup Language
+
+### [Custom containers](https://github.com/markdown-it/markdown-it-container)
+
+::: warning
+*here be dragons*
+:::"""
