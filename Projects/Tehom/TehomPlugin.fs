a6ae3640d6ef02af7ce47f6a31e4d5eef7e55aa8
yvileapsis <yvileapsis@gmail.com>
2024-07-11 22:03:23 +0300

    restarting since upstream broke history

---
+++ Projects/Tehom/TehomPlugin.fs
@@ -0,0 +1,24 @@
+﻿namespace Tehom
+open System
+open Nu
+
+// this is a plugin for the Nu game engine that directs the execution of your application and editor
+type TehomPlugin () =
+    inherit NuPlugin ()
+
+    // this exposes different editing modes in the editor
+    override this.EditModes =
+        Map.ofList [
+            "Splash", Game.SetMyGame Splash
+            "Title", Game.SetMyGame Title
+            "Credits", Game.SetMyGame Credits
+            "Gameplay", fun world ->
+                let world = Simulants.Gameplay.SetGameplay Gameplay.initial world
+                let world = Game.SetMyGame Gameplay world
+                world
+        ]
+
+    // this specifies which packages are automatically loaded at game start-up.
+    override this.InitialPackages = [
+        Assets.Gui.PackageName
+    ]
