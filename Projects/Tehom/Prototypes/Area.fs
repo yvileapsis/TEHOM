64c08b46edbd59b332a6c2d1e6c95d3d4b821be5
yvileapsis <yvileapsis@gmail.com>
2024-09-26 22:49:06 +0300

    wip

---
+++ Projects/Tehom/Prototypes/Area.fs
@@ -0,0 +1,174 @@
+namespace Tehom
+
+open System
+open System.Numerics
+open Prime
+open Nu
+open FGL
+
+// TODO: merge graphs
+// TODO: moving in graph
+
+(*
+* 5 Rooms:
+* Waitroom, barricaded windows, rolling hospital bed, locked exit door, you wake up here
+* Registration room, safe with useful stuff like a pistol maybe
+* Main hall
+* Electrical room to fix the lights, rat attacks
+* Surgery room, surgery table, dog on the table, note (dog ate the key), opening it lets spider chandalier escape
+* Goal is to go to surgery room, take the key, return to waitroom, open the door and exit.
+* Optionally you can fix lights and fight spider chandalier.
+*)
+
+
+module Area =
+
+    type Site =
+        | Site
+    with
+        static member empty = Site
+
+    type Relationship =
+        | Distance of Distance: float
+        | Consists
+        | Contains
+        | LiesAbove
+        | Covers
+        | OnEdge
+
+    type Sites = Graph<String, Site, Relationship>
+
+    type Area = {
+        Name : string
+        Sites : Sites
+    }
+    with
+        static member empty = {
+            Name = String.empty
+            Sites = Graph.empty
+        }
+
+        // * Waitroom, barricaded windows, rolling hospital bed, locked exit door, you wake up here
+        static member room1 : Sites =
+            Graph.empty
+            // room itself
+            |> Vertices.add ("room1", Site.empty)
+            // room parts
+            |> Vertices.add ("room1floor", Site.empty)
+            |> Vertices.add ("room1ceiling", Site.empty)
+            |> Vertices.add ("room1walls", Site.empty)
+            |> Vertices.add ("room1corners", Site.empty)
+            |> Vertices.add ("room1air", Site.empty)
+            // room consists of its parts
+            |> Directed.Edges.add ("room1", "room1floor", Consists)
+            |> Directed.Edges.add ("room1", "room1ceiling", Consists)
+            |> Directed.Edges.add ("room1", "room1walls", Consists)
+            |> Directed.Edges.add ("room1", "room1corners", Consists)
+            |> Directed.Edges.add ("room1", "room1air", Consists)
+            // room size
+            |> Undirected.Edges.add ("room1air", "room1floor", Distance 150)
+            |> Undirected.Edges.add ("room1air", "room1ceiling", Distance 100)
+            |> Undirected.Edges.add ("room1air", "room1walls", Distance 600)
+            |> Directed.Edges.add ("room1corners", "room1floor", OnEdge)
+            // actors inside the room
+            |> Vertices.add ("room1barricadedWindow1", Site.empty)
+            |> Vertices.add ("room1barricadedWindow2", Site.empty)
+            |> Vertices.add ("room1exitFinal", Site.empty)
+            |> Vertices.add ("room1exitMainHall", Site.empty)
+            |> Vertices.add ("room1gurney", Site.empty)
+            // locations of actors inside the room
+            |> Directed.Edges.add ("room1walls", "room1barricadedWindow1", OnEdge)
+            |> Directed.Edges.add ("room1walls", "room1barricadedWindow2", OnEdge)
+            |> Directed.Edges.add ("room1walls", "room1exitFinal", OnEdge)
+            |> Directed.Edges.add ("room1walls", "room1exitMainHall", OnEdge)
+            |> Directed.Edges.add ("room1floor", "room1gurney", LiesAbove)
+
+            // characters inside the room
+            |> Vertices.add ("player", Site.empty)
+            |> Directed.Edges.add ("room1gurney", "player", LiesAbove)
+
+
+        // main hall, rat
+        static member room2 : Sites =
+            Graph.empty
+            |> Vertices.add ("room2", Site.empty)
+
+            |> Vertices.add ("room2floor", Site.empty)
+            |> Vertices.add ("room2ceiling", Site.empty)
+            |> Vertices.add ("room2walls", Site.empty)
+            |> Vertices.add ("room2corners", Site.empty)
+            |> Vertices.add ("room2air", Site.empty)
+
+            |> Directed.Edges.add ("room2", "room2floor", Consists)
+            |> Directed.Edges.add ("room2", "room2ceiling", Consists)
+            |> Directed.Edges.add ("room2", "room2walls", Consists)
+            |> Directed.Edges.add ("room2", "room2corners", Consists)
+            |> Directed.Edges.add ("room2", "room2air", Consists)
+
+            |> Undirected.Edges.add ("room2air", "room2floor", Distance 200)
+            |> Undirected.Edges.add ("room2air", "room2ceiling", Distance 200)
+            |> Undirected.Edges.add ("room2air", "room2walls", Distance 600)
+            |> Directed.Edges.add ("room2corners", "room2floor", OnEdge)
+
+            |> Vertices.add ("room2table1", Site.empty)
+            |> Vertices.add ("room2table2", Site.empty)
+            |> Vertices.add ("room2table3", Site.empty)
+            |> Vertices.add ("room2exitWaitingRoom", Site.empty)
+
+            |> Directed.Edges.add ("room2walls", "room2exitWaitingRoom", LiesAbove)
+            |> Directed.Edges.add ("room2floor", "room2table1", LiesAbove)
+            |> Directed.Edges.add ("room2floor", "room2table2", LiesAbove)
+            |> Directed.Edges.add ("room2floor", "room2table3", LiesAbove)
+
+            |> Vertices.add ("rat", Site.empty)
+            |> Directed.Edges.add ("room2floor", "player", LiesAbove)
+
+        static member level1 = {
+            Name = "Clinic"
+            Sites =
+                Graph.empty
+                |> Area.room1
+                |> Area.room2
+                |> Undirected.Edges.add ("room1exitMainHall", "room2exitWaitingRoom", OnEdge)
+
+        }
+
+        static member find finder (level : Area) =
+            level.Sites
+            |> Vertices.toVertexList
+            |> List.tryFind finder
+            |> function | Some v -> Some (fst v) | None -> None
+
+        static member findSite site (area : Area) =
+            Area.find (fun (string, vertex) -> string = site) area
+
+        static member findPath fromDestination toDestination level =
+            level.Sites
+            |> Undirected.Edges.map (fun v1 v2 edge ->
+                match edge with
+                | Distance x -> uint x
+                | Consists -> UInt32.MaxValue
+                | Contains -> 0u
+                | LiesAbove -> 0u
+                | Covers -> 0u
+                | OnEdge -> 0u
+            )
+            |> Query.sp fromDestination toDestination
+
+        static member moveActor actor fromDestination toDestination level =
+            if not (fromDestination = toDestination) then
+                {
+                    level with
+                        Sites = Vertices.map (fun string vertex ->
+                            if string = fromDestination then
+                                { vertex with Actors = Set.remove actor vertex.Actors }
+                            elif string = toDestination then
+                                { vertex with Actors = Set.add actor vertex.Actors }
+                            else
+                                vertex
+                        ) level.Sites
+                }
+            else
+                level
+
+type Area = Area.Area
---
+++ Projects/Tehom/Prototypes/Character.fs
@@ -0,0 +1,269 @@
+namespace Tehom
+
+open System
+open Prime
+
+module Character =
+
+    type Element =
+        | Gall
+        | Lymph
+        | Oil
+        | Plasma
+
+    type MajorWounds =
+        | Healthy = 0
+        | Hurt = 1
+        | Bruised = 2
+        | Wounded = 3
+        | Injured = 4
+        | Critical = 5
+        | Down = 6
+        | Dying = 7
+        | Dead = 8
+
+    type Stat =
+        | Crippled = 0
+        | Feeble = 1
+        | Poor = 2
+        | Average = 3
+        | Gifted = 4
+        | Superior = 5
+        | Exceptional = 6
+        | Transcendent = 7
+        | Divine = 8
+        | Impossible = 9
+    // C F P A S G E T D I
+
+    type Gait =
+        | Sluggish
+        | Slow
+        | Moderate
+        | Fast
+        | Speedy
+    with
+        static member multiplier = function
+            | Sluggish -> 0.5
+            | Slow -> 0.75
+            | Moderate -> 1.0
+            | Fast -> 1.25
+            | Speedy -> 1.5
+
+    type Stats = Stat * Stat * Stat * Stat
+
+    type Stance = int * int * int * int
+
+    type Character = {
+        // Static stats
+        ID : String
+        Name : String
+        Stats : Stats
+        Edges : Stat list
+        Stances : int
+
+        // Dynamic stats
+        Damage : int
+        Size : int
+        Gait : Gait
+
+        MajorWounds : MajorWounds
+        MinorWounds : int
+
+        Initiative : int
+
+        Stance : Stance
+        StancesLeft : int
+    }
+    with
+        static member getElement element stats =
+            let (gall, lymph, oil, plasma) = stats
+            match element with
+            | Gall -> gall
+            | Lymph -> lymph
+            | Oil -> oil
+            | Plasma -> plasma
+
+        static member statsEmpty : Stats = Stat.Crippled, Stat.Crippled, Stat.Crippled, Stat.Crippled
+        static member stanceEmpty : Stance = 0, 0, 0, 0
+        static member stanceMove toStat fromStat (stance : Stance) =
+
+            let moveOne toStat fromStat stance =
+                let (gall, lymph, oil, plasma) = stance
+
+                let stance : Stance =
+                    match fromStat with
+                    | Gall -> gall - 1, lymph, oil, plasma
+                    | Lymph -> gall, lymph - 1, oil, plasma
+                    | Oil -> gall, lymph, oil - 1, plasma
+                    | Plasma-> gall, lymph, oil, plasma - 1
+
+                let (gall, lymph, oil, plasma) = stance
+
+                let stance : Stance =
+                    match toStat with
+                    | Gall -> gall + 1, lymph, oil, plasma
+                    | Lymph -> gall, lymph + 1, oil, plasma
+                    | Oil -> gall, lymph, oil + 1, plasma
+                    | Plasma-> gall, lymph, oil, plasma + 1
+
+                stance
+
+            List.zip toStat fromStat
+            |> List.fold (fun stance (toStat, fromStat) -> moveOne toStat fromStat stance) stance
+
+        static member stanceVerify (stance : Stance) (stats : Stats) =
+            let (gall, lymph, oil, plasma) = stats
+            let (gallChange, lymphChange, oilChange, plasmaChange) = stance
+            gallChange + lymphChange + oilChange + plasmaChange |> int = 0
+            && - int gallChange < int gall
+            && - int lymphChange < int lymph
+            && - int oilChange < int oil
+            && - int plasmaChange < int plasma
+
+        static member stanceChange (stance : Stance) character =
+            if (character.StancesLeft > 0) then
+                {
+                    character with
+                        Stance = stance
+                        StancesLeft = character.StancesLeft - 1
+                }
+            else
+                character
+
+        static member stanceReset (character : Character) =
+            {
+                character with
+                    Stance = Character.stanceEmpty
+                    StancesLeft = character.Stances
+            }
+
+        static member getStats character : Stats =
+            character.Stats
+
+        static member getStancedStats character : Stats =
+            let (gall, lymph, oil, plasma) = character.Stats
+            let (gallChange, lymphChange, oilChange, plasmaChange) = character.Stance
+            gall + enum gallChange, lymph + enum lymphChange, oil + enum oilChange, plasma + enum plasmaChange
+
+        static member getStat element character =
+            character.Stats
+            |> Character.getElement element
+            |> int
+
+        static member getStancedStat element character =
+            character
+            |> Character.getStancedStats
+            |> Character.getElement element
+            |> int
+
+        static member getDamage character =
+            character.Damage
+
+        static member doDamage damage character =
+            let (_, lymph, oil, _) = Character.getStats character
+
+            let lymph = int lymph
+            let oil = int oil
+
+            let maxDamage newValue =
+                 MajorWounds.Dead
+                 |> int
+                 |> min newValue
+                 |> enum
+
+            if (damage < lymph) then character
+            else if (damage < 2 * lymph) then
+                if (character.MinorWounds < oil) then
+                    { character with MinorWounds = character.MinorWounds + 1 }
+                else
+                    { character with MajorWounds = maxDamage (1 + int character.MajorWounds) }
+            else if (damage < 3 * (int lymph)) then
+                { character with MajorWounds = maxDamage (1 + int character.MajorWounds) }
+            else if (damage < 4 * lymph) then
+                { character with MajorWounds = maxDamage (2 + int character.MajorWounds) }
+            else if (damage < 5 * lymph) then
+                { character with MajorWounds = maxDamage (3 + int character.MajorWounds) }
+            else if (damage < 6 * lymph) then
+                { character with MajorWounds = maxDamage (4 + int character.MajorWounds) }
+            else if (damage < 7 * lymph) then
+                { character with MajorWounds = maxDamage (5 + int character.MajorWounds) }
+            else
+                { character with MajorWounds = maxDamage (6 + int character.MajorWounds) }
+
+        static member getMaxInitiative character =
+            let (gall, _, _, plasma) = Character.getStats character
+            int gall, int plasma
+
+        static member getSpeed character =
+            let multiplier = Gait.multiplier character.Gait
+            let baseSpeed =
+                // mouse to beagle
+                if character.Size <= -2 then
+                    5.0
+                // human child
+                elif character.Size = -1 then
+                    10.0
+                // human
+                elif character.Size = 0 then
+                    20.0
+                // bear
+                elif character.Size = 1 then
+                    30.0
+                // elephant
+                elif character.Size = 2 then
+                    40.0
+                // blue whale
+                elif character.Size = 3 then
+                    50.0
+                //
+                elif character.Size = 4 then
+                    60.0
+                // leviathan
+                elif character.Size = 5 then
+                    70.0
+                else
+                    70.0
+            baseSpeed * multiplier
+
+        static member empty = {
+            ID = String.empty
+            Name = String.empty
+            Stats = Character.statsEmpty
+            Stance = Character.stanceEmpty
+
+            Damage = 0
+            Size = 0
+            Gait = Moderate
+
+            Edges = []
+
+            Stances = 0
+            StancesLeft = 0
+
+            MajorWounds = MajorWounds.Healthy
+            MinorWounds = 0
+
+            Initiative = 0
+        }
+
+        static member player = {
+            Character.empty with
+                ID = "player"
+                Name = "Player"
+                Stats = Stat.Gifted, Stat.Gifted, Stat.Gifted, Stat.Gifted
+
+                Damage = 5
+                Stances = 2
+        }
+
+        static member enemy = {
+            Character.empty with
+                ID = "enemy"
+                Name = "Enemy"
+                Stats = Stat.Average, Stat.Average, Stat.Average, Stat.Average
+
+                Damage = 5
+                Stances = 1
+        }
+
+type Character = Character.Character
