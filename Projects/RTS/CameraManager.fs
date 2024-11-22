7edf657da57b4ed21c52b7c43a4b082cc272bd1b
yvileapsis <yvileapsis@gmail.com>
2024-11-22 15:49:12 +0300

    Refactoring CameraManager

--- Projects/RTS/CameraManager.fs
+++ Projects/RTS/CameraManager.fs
@@ -5,53 +5,72 @@ open System.Numerics
 open Prime
 open Nu
 
+type CameraRotation = {
+    PositionStart : float32
+    PositionLast : float32
+}
+
+type CameraQuickMove = {
+    PositionStart : Vector2
+}
+
 type [<ReferenceEquality; SymbolicExpansion>] CameraManager = {
       SpeedMinimum : float32
       SpeedMaximum : float32
+      QuickMoveMargin : Vector2
+
       RotationSpeed : float32
 
       Angle : float32
+
       DistanceMinimum : float32
       DistanceMaximum : float32
 
       ZoomCurrent : float32
       ZoomStep : float32
 
-      Rotating : (float32 * float32) option
-      FastMoving : Vector2 option
-      FastMovingDistanceMinimum : Vector2
+      ActiveRotation : CameraRotation option
+      ActiveQuickMove : CameraQuickMove option
 }
 with
     static member initial = {
         SpeedMinimum = 0.5f
         SpeedMaximum = 2.0f
+        QuickMoveMargin = v2 4f 4f
+
         RotationSpeed = 0.1f
 
         Angle = 45f
+
         DistanceMinimum = 4f
         DistanceMaximum = 12f
+
         ZoomCurrent = 1.0f
         ZoomStep = 0.05f
 
-        Rotating = None
-        FastMoving = None
-        FastMovingDistanceMinimum = v2 4f 4f
+        ActiveRotation = None
+        ActiveQuickMove = None
     }
 
 type CameraManagerMessage =
     | Update
     | MouseMove of MouseMoveData
-    | Zoom of MouseWheelData
-    | RotateClickDown of MouseButtonData
-    | RotateClickUp of MouseButtonData
-    | RotateMove of MouseMoveData
-    | FastMoveClickDown of MouseButtonData
-    | FastMoveClickUp of MouseButtonData
+    | MouseButton of MouseButtonData
+    | MouseWheel of MouseWheelData
+    | Move
+    | Zoom of float32
+    | RotationStart
+    | RotationProcess of CameraRotation
+    | RotationFinish of CameraRotation
+    | QuickMoveStart
+    | QuickMoveProcess of CameraQuickMove
+    | QuickMoveFinish
     interface Message
 
 type CameraManagerCommand =
     | Register
-    | UpdateTransform of Vector3 * Quaternion
+    | SetTransform of Vector3 * Quaternion
+    | AddTransform of Vector3 * Quaternion
     interface Command
 
 [<AutoOpen>]
@@ -65,28 +84,59 @@ module CameraManagerExtensions =
 type CameraManagerDispatcher () =
     inherit Entity3dDispatcher<CameraManager, CameraManagerMessage, CameraManagerCommand> (false, false, false, CameraManager.initial)
 
-    override this.Definitions (character, _) = [
+    override this.Definitions (_, _) = [
         Entity.Rotation == Quaternion.CreateFromYawPitchRoll (Math.DegreesToRadians 45f, 0f, 0f)
         Entity.UpdateEvent => Update
-        Game.MouseWheelEvent =|> fun evt -> Zoom evt.Data
-        Game.MouseMiddleDownEvent =|> fun evt -> RotateClickDown evt.Data
-        Game.MouseMiddleUpEvent =|> fun evt -> RotateClickUp evt.Data
+        Game.MouseWheelEvent =|> fun evt -> MouseWheel evt.Data
         Game.MouseMoveEvent =|> fun evt -> MouseMove evt.Data
-        Game.MouseRightDownEvent =|> fun evt -> FastMoveClickDown evt.Data
-        Game.MouseRightUpEvent =|> fun evt -> FastMoveClickUp evt.Data
+        Game.MouseMiddleDownEvent =|> fun evt -> MouseButton evt.Data
+        Game.MouseMiddleUpEvent =|> fun evt -> MouseButton evt.Data
+        Game.MouseRightDownEvent =|> fun evt -> MouseButton evt.Data
+        Game.MouseRightUpEvent =|> fun evt -> MouseButton evt.Data
     ]
 
-    override this.Message (character, message, entity, world) =
+    override this.Message (model, message, entity, world) =
 
         match message with
         | Update ->
+            let signals : Signal list = [
+                Move
+                if model.ActiveQuickMove.IsSome then QuickMoveProcess model.ActiveQuickMove.Value
+            ]
 
-            let position = entity.GetPosition world
+            withSignals signals model
+
+        | MouseButton mouseButtonData ->
+            let signals : Signal list = [
+                if mouseButtonData.Button = MouseMiddle && mouseButtonData.Down then RotationStart
+                if mouseButtonData.Button = MouseMiddle && not mouseButtonData.Down && model.ActiveRotation.IsSome
+                then RotationFinish model.ActiveRotation.Value
+                if mouseButtonData.Button = MouseRight && mouseButtonData.Down then QuickMoveStart
+                if mouseButtonData.Button = MouseRight && not mouseButtonData.Down then QuickMoveFinish
+            ]
+
+            withSignals signals model
+
+        | MouseMove _ ->
+            let signals : Signal list = [
+                if model.ActiveRotation.IsSome then RotationProcess model.ActiveRotation.Value
+            ]
+
+            withSignals signals model
+
+        | MouseWheel mouseWheelData ->
+            let signals : Signal list = [
+                Zoom mouseWheelData.Travel
+            ]
+
+            withSignals signals model
+
+        | Move ->
             let rotation = entity.GetRotation world
 
             let forward = rotation.Forward
             let right = rotation.Right
-            let speed = character.SpeedMinimum
+            let speed = model.SpeedMinimum
 
             let walkVelocity =
                 (if World.isKeyboardKeyDown KeyboardKey.W world then forward * speed else v3Zero) +
@@ -94,134 +144,119 @@ type CameraManagerDispatcher () =
                 (if World.isKeyboardKeyDown KeyboardKey.A world then -right * speed else v3Zero) +
                 (if World.isKeyboardKeyDown KeyboardKey.D world then right * speed else v3Zero)
 
-            let position = if walkVelocity <> v3Zero then position + walkVelocity else position
+            let position = if walkVelocity <> v3Zero then walkVelocity else v3Zero
 
-            let position =
-                match character.FastMoving with
-                | Some positionStart ->
-
-                    let mousePosition = World.getMousePosition2dScreen world
-
-                    let speed =
-                        if ((mousePosition - positionStart).Length () > character.FastMovingDistanceMinimum.Length ()) then
-                            0.01f * character.SpeedMinimum * (mousePosition - positionStart)
-                        else
-                            v2Zero
-
-                    let walkVelocity =
-                        forward * speed.Y + right * speed.X
-
-                    let position = if walkVelocity <> v3Zero then position + walkVelocity else position
-
-                    position
-                | None ->
-                    position
-
-            let turnSpeed = character.RotationSpeed
+            let turnSpeed = model.RotationSpeed
 
             let turnVelocity =
                 (if World.isKeyboardKeyDown KeyboardKey.Right world then -turnSpeed else 0.0f) +
                 (if World.isKeyboardKeyDown KeyboardKey.Left world then turnSpeed else 0.0f)
 
-            let rotation = if turnVelocity <> 0.0f then rotation * Quaternion.CreateFromAxisAngle (v3Up, turnVelocity) else rotation
+            let rotation = if turnVelocity <> 0.0f then Quaternion.CreateFromAxisAngle (v3Up, turnVelocity) else Quaternion.Identity
 
-            withSignals [UpdateTransform (position, rotation)] character
+            let signals : Signal list = [
+                AddTransform (position, rotation)
+            ]
 
-        | MouseMove mouseMoveData ->
-            withSignals [RotateMove mouseMoveData] character
+            withSignals signals model
 
-        | RotateClickDown _ ->
-            let mousePosition = World.getMousePosition2dScreen world
-
-            let character = { character with Rotating = Some (mousePosition.X, mousePosition.X) }
-
-            just character
-
-        | RotateClickUp _ ->
-            match character.Rotating with
-            | Some (positionStart, _) ->
-                let position = entity.GetPosition world
-
-                let mousePosition = World.getMousePosition2dScreen world
-
-                if (positionStart = mousePosition.X) then
-                    let character = { character with Rotating = None; ZoomCurrent = 1f }
-
-                    let rotation = Quaternion.CreateFromYawPitchRoll (Math.DegreesToRadians 45f, 0f, 0f)
-
-                    withSignals [UpdateTransform (position, rotation)] character
-                else
-                    let character = { character with Rotating = None }
-
-                    just character
-
-            | None ->
-                just character
-
-        | RotateMove _ ->
-            match character.Rotating with
-            | Some (positionStart, positionLast) ->
-                let position = entity.GetPosition world
-                let rotation = entity.GetRotation world
-
-                let turnSpeed = character.RotationSpeed
-
-                let mousePosition = World.getMousePosition2dScreen world
-
-                let turnVelocity =
-                    (mousePosition.X - positionLast) * turnSpeed * 0.1f
-
-                let rotation = if turnVelocity <> 0.0f then rotation * Quaternion.CreateFromAxisAngle (v3Up, turnVelocity) else rotation
-
-                let character = { character with Rotating = Some (positionStart, mousePosition.X) }
-
-                withSignals [UpdateTransform (position, rotation)] character
-
-            | None ->
-                just character
-        | FastMoveClickDown _ ->
-            let mousePosition = World.getMousePosition2dScreen world
-
-            let character = { character with FastMoving = Some mousePosition }
-
-            just character
-
-        | FastMoveClickUp _ ->
-
-            let character = { character with FastMoving = None }
-
-            just character
-
-        | Zoom mouseWheelData ->
-
-            let zoom = character.ZoomCurrent
-            let step = character.ZoomStep
-
-            let zoomVelocity =
-                mouseWheelData.Travel
+        | Zoom travel ->
+            let zoom = model.ZoomCurrent
+            let step = model.ZoomStep
 
             let zoom =
-                zoom - zoomVelocity * step
+                zoom - travel * step
                 |> min 1f
                 |> max 0f
 
-            let character = {
-                character with
-                    ZoomCurrent = zoom
-            }
-
+            let character = { model with ZoomCurrent = zoom }
             just character
 
+        | RotationStart ->
+            let mousePosition = World.getMousePosition2dScreen world
+            let activeRotation = { PositionStart = mousePosition.X; PositionLast = mousePosition.X}
+            let character = { model with ActiveRotation = Some activeRotation }
+            just character
 
+        | RotationProcess activeRotation ->
+            let turnSpeed = model.RotationSpeed
 
-    override this.Command (character, command, entity, world) =
+            let mousePosition = World.getMousePosition2dScreen world
+
+            let turnVelocity =
+                (mousePosition.X - activeRotation.PositionLast) * turnSpeed * 0.1f
+
+            let signals : Signal list = [
+                if turnVelocity <> 0.0f then AddTransform (v3Zero, Quaternion.CreateFromAxisAngle (v3Up, turnVelocity))
+            ]
+
+            let activeRotation = { activeRotation with PositionLast = mousePosition.X }
+
+            let character = { model with ActiveRotation = Some activeRotation }
+
+            withSignals signals character
+
+        | RotationFinish activeRotation ->
+            let mousePosition = World.getMousePosition2dScreen world
+
+            if (activeRotation.PositionStart = mousePosition.X) then
+                let character = { model with ActiveRotation = None; ZoomCurrent = 1f }
+
+                let position = entity.GetPosition world
+                let rotation = Quaternion.CreateFromYawPitchRoll (Math.DegreesToRadians 45f, 0f, 0f)
+
+                withSignals [SetTransform (position, rotation)] character
+            else
+                let character = { model with ActiveRotation = None }
+
+                just character
+
+        | QuickMoveStart ->
+            let mousePosition = World.getMousePosition2dScreen world
+            let activeQuickMove = { PositionStart = mousePosition }
+            let character = { model with ActiveQuickMove = Some activeQuickMove }
+            just character
+
+        | QuickMoveProcess activeQuickMove ->
+            let rotation = entity.GetRotation world
+
+            let forward = rotation.Forward
+            let right = rotation.Right
+
+            let mousePosition = World.getMousePosition2dScreen world
+
+            let speed =
+                if ((mousePosition - activeQuickMove.PositionStart).Length () > model.QuickMoveMargin.Length ()) then
+                    0.01f * model.SpeedMinimum * (mousePosition - activeQuickMove.PositionStart)
+                else
+                    v2Zero
+
+            let signals : Signal list = [
+                if speed <> v2Zero then AddTransform (forward * speed.Y + right * speed.X, Quaternion.Identity)
+            ]
+
+            withSignals signals model
+
+        | QuickMoveFinish ->
+            let character = { model with ActiveQuickMove = None }
+            just character
+
+    override this.Command (_, command, entity, world) =
 
         match command with
         | Register ->
             just world
-        | UpdateTransform (position, rotation) ->
+
+        | SetTransform (position, rotation) ->
             let world = entity.SetPosition position world
             let world = entity.SetRotation rotation world
+            just world
+
+        | AddTransform (positionDelta, rotationDelta) ->
+            let position = entity.GetPosition world
+            let rotation = entity.GetRotation world
+            let world = entity.SetPosition (position + positionDelta) world
+            let world = entity.SetRotation (rotation * rotationDelta) world
             just world
 
     override this.Content (character, entity) = [
